e04826120f7e43cbec53484a8caff547e0f5680f
hasinimadhuwnthi <hasinimadhuwnthi1234@gmail.com>
2020-05-03 09:59:23 +0530

    First commit

---
+++ Assets/EyeRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeRotation : MonoBehaviour
+{
+    float x;
+    void Update()
+    {
+	x += Time.deltaTime * 10;
+	transform.rotation = Quaternion.Euler(x,0,0);
+    }
+}
